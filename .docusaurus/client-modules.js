1e387aeb94d0ebea18bc8ce706348d736a089fd2
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-08-12 12:41:25 +1000

    Deploy website - based on ac408020c3c94d58b2b146456c21dc96c3e5f497

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/axite/code/jailbreak_wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/axite/code/jailbreak_wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/axite/code/jailbreak_wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/axite/code/jailbreak_wiki/src/css/custom.css"),
-];
--- docs/Hardware/Sony/Playstation/4/Firmware Version/11.0/GoldHEN.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# GoldHEN Exploit (2.4b17.3)
-
-## TODO: Complete document
