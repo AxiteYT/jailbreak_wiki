23b679589fbad6ee3b133df47c358d0673ad0248
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-08-12 13:14:54 +1000

    Revert baseURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: "https://jailbreak.wiki",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/jailbreak_wiki/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
